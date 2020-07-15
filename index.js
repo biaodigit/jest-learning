4d0328f82a031dc39da539270a40d9d527c0f90f
biaodigit <15622285015@163.com>
2020-07-15 22:35:32 +0800

    test

---
+++ index.js
@@ -0,0 +1,18 @@
+export function add(a, b) {
+    return a + b
+}
+
+export function minus(a, b) {
+    return a - b
+}
+
+export function multi(a, b) {
+    return a * b
+}
+
+// module.exports = {
+//     add,
+//     minus,
+//     multi
+// }
+
---
+++ index2.test.js
@@ -0,0 +1,12 @@
+test('测试对象相等', () => {
+    const a = { b: 1 }
+    expect(a).toEqual({ b: 1 })
+})
+
+const throwError = () => {
+    throw new Error('this is a new error')
+}
+
+test('toThrow',() => {
+    expect(throwError).toThrow(/this is an new error/)
+})
